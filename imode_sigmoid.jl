889f7cd3a1a1b31bc4df9bdb6dbea0d7f80108dd
Loris Mendolia <lmendolia@uliege.be>
2024-04-09 14:34:56 +0200

    Copied sigmoid solving code into a module

---
+++ imode_sigmoid.jl
@@ -0,0 +1,65 @@
+module ImodeSigmoid
+
+using BifurcationKit, Plots, Parameters, NLsolve
+const BK = BifurcationKit
+
+const I0 = 1e-12 # A
+const κ = 0.7
+const Vdd = 1.8 # V
+const UT= 25*1e-3 # V
+const C=1e-3 # F
+
+# Static transistor equations
+Icmp(Vin,V1) = I0 * exp(κ*(Vdd-Vin)/UT) * (1 - exp(-(Vdd-V1)/UT))
+I1(Vthr,V1) = I0 * exp(κ*Vthr/UT) * (1 - exp(-V1/UT))
+Ilin1(V1,Vout) = I0 * exp((κ*V1 - Vout)/UT) * (1 - exp(-(V1-Vout)/UT))
+Ilin2(Vout,V2) = I0 * exp((κ*Vout - V2)/UT) * (1 - exp(-(Vout-V2)/UT))
+Ilin3(Vlin,V2) = I0 * exp(κ*Vlin/UT) * (1 - exp(-V2/UT));
+
+function Imode_sigmoid_V(x,pars)
+
+	@unpack Vin, Vlin, Vthr = pars
+
+    V1, Vout, V2 = x
+
+    [Icmp(Vin,x[1]) - I1(Vthr,x[1]) - Ilin1(x[1],x[2]),
+    Ilin1(x[1],x[2]) - Ilin2(x[2],x[3]),
+    Ilin2(x[2],x[3]) - Ilin3(Vlin,x[3])] ./C
+    
+end
+
+V_P_diode(I) = Vdd - UT/κ * log(I/I0)
+V_N_diode(I) = UT/κ * log(I/I0)
+
+Iin(Vin) = I0 * exp(κ*(Vdd-Vin)/UT)
+Iout(Vout, Vgain) = I0 * exp((κ*Vout)/UT) / (1 + exp(κ*(Vout-Vgain)/UT))
+
+function Imode_sigmoid_sim(Iin_range, params)
+
+	@unpack Ithr, Igain, Ilin = params
+
+	# Current mirror parameter conversion
+	Vthr = V_N_diode.(Ithr)
+	Vgain = V_N_diode(Igain)
+	Vlin = V_N_diode.(Ilin)
+	Vin = V_P_diode.(Igain) # We want to make an initial guess with a high current to avoid very small values for our variables
+
+	pars_V = (Vin = Vin, Vlin = Vlin, Vthr = Vthr)
+
+	x0=[1.7, 0.9, 0.3]
+
+	solNL = nlsolve(x -> Imode_sigmoid_V(x,pars_V), x0, iterations=convert(Int64,1e6), ftol=1e-9, xtol=1e-6)
+
+	rfs(x, p) = (x2 = x[2], y=p) # Record the output voltage
+	prob = BifurcationProblem(Imode_sigmoid_V, solNL.zero, pars_V, (@lens _.Vin), record_from_solution = rfs) # Set up the bifurcation problem with the input voltage as the bifurcation parameter
+
+	# We set up continuation to use the selected input current range, by converting them to PMOS current mirror voltages
+	opts = ContinuationPar(p_min = V_P_diode(Iin_range[2]), p_max = V_P_diode(Iin_range[1]), n_inversion = 50, ds = 1e-6, dsmin = 1e-12, dsmax = 1e-3, max_steps = 1000, nev = 3)
+	br = continuation(prob, PALC(), opts; normC = norminf, bothside = true)
+
+	return (Iin.(br.branch.param), Iout.(br.branch.x2, Vgain))
+end
+
+export Imode_sigmoid_sim
+
+end
