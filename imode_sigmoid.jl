35bbcf3f76bfa562216f3ffa8b1ae1f1394fde1a
Loris Mendolia <lorismendolia@hotmail.com>
2024-09-24 11:52:13 +0200

    Replaced lens keyword for compatibility with newer BifurcationKit version

--- imode_sigmoid.jl
+++ imode_sigmoid.jl
@@ -75,7 +75,7 @@ function Imode_sigmoid_sim(Iin_range, params; return_V = false)
 	solNL = nlsolve(x -> Imode_sigmoid_V(x,pars_V), x0, iterations=convert(Int64,1e6), ftol=1e-9, xtol=1e-6)
 
 	rfs(x, p) = (x2 = x[2], y=p) # Record the output voltage
-	prob = BifurcationProblem(Imode_sigmoid_V, solNL.zero, pars_V, (@lens _.Vin), record_from_solution = rfs) # Set up the bifurcation problem with the input voltage as the bifurcation parameter
+	prob = BifurcationProblem(Imode_sigmoid_V, solNL.zero, pars_V, (@optic _.Vin), record_from_solution = rfs) # Set up the bifurcation problem with the input voltage as the bifurcation parameter
 
 	# We set up continuation to use the selected input current range, by converting them to PMOS current mirror voltages
 	opts = ContinuationPar(p_min = V_P_diode(Iin_range[2]), p_max = V_P_diode(Iin_range[1]), n_inversion = 50, ds = 1e-6, dsmin = 1e-12, dsmax = 1e-3, max_steps = 1000, nev = 3)
